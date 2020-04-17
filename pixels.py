79656ca55ba839b0ccebd021037d46c019bfd96f
Bikash <oksbwn@gmail.com>
2020-04-17 20:20:16 +0100

    Fixed LED Speak state for errors

--- pixels.py
+++ pixels.py
@@ -3,7 +3,10 @@ LED light pattern like Google Home
 """
 from mycroft_bus_client import MessageBusClient, Message
 from effects import PixelEffects
+import threading
 
+
+timer = None
 pixels = PixelEffects()
 client =MessageBusClient()
 
@@ -11,14 +14,19 @@ def wakeword_dtected(message):
     pixels.wakeup()
     pixels.listen()
 
-def audio_output_start(message):
-    pixels.speak()
-
 def m_ready(message):
     pixels.off()
 
 def record_end(message):
     pixels.think()
+    global timer
+    timer = threading.Timer(10.0,pixels.off) 
+    timer.start()
+
+def audio_output_start(message):
+    global timer
+    timer.cancel()
+    pixels.speak()
 
 if __name__ == '__main__':
     pixels.wakeup()
@@ -28,4 +36,5 @@ if __name__ == '__main__':
     client.on('recognizer_loop:record_end',record_end)
     client.on('recognizer_loop:audio_output_start',audio_output_start)
     client.on('recognizer_loop:audio_output_end',m_ready)
+    client.on('recognizer_loop:sleep',m_ready)
     client.run_forever()
